63fa04e5fb3d7d12d4683f5c147d075cd3a9860d
Carlos <carlos.oliveirajr@hotmail.com>
2025-08-12 21:45:52 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,11 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
+	
 	@Override 
 	public void start(Stage primaryStage) { 
 	 try { 
@@ -19,13 +24,17 @@ public class Main extends Application {
 	  scrollPane.setFitToWidth(true);
 	  
 	  
-	  Scene mainScene = new Scene(scrollPane); 
+	  mainScene = new Scene(scrollPane); 
 	  primaryStage.setScene(mainScene); 
 	  primaryStage.setTitle("ESTOQUE APP VERSÃO 0.0.1"); 
 	  primaryStage.show(); 
 	 } catch (IOException e) { 
 	  e.printStackTrace(); 
 	 } 
+	}
+	
+	public static Scene getMainScene(){
+		return mainScene;
 	}
 	
 	
